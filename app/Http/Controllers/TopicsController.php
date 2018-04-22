946e308213aff2881429cfdf6b384c21df1dea42
k155 <hy_kissss@163.com>
2018-04-22 08:06:02 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,9 +14,10 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-        $topics = Topic::with('user', 'category')->paginate(30);
+    public function index(Request $request, Topic $topic)
+    {
+
+        $topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Ki55</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://github.com/hykissss/larabbs" target="_blank">Ki55</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="pull-right"><a href="mailto:hyuuu1024@gmail.com">联系我们</a></p>
