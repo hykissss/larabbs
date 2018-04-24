96dd3b6ebd9fdee7f398e762db4474e04f4248d9
k155 <hy_kissss@163.com>
2018-04-24 03:33:24 +0000

    Admin can manage content

--- config/administrator.php
+++ config/administrator.php
@@ -41,7 +41,11 @@ return array(
             'users',
             'roles',
             'permissions',
-            
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
         ],
     ],
 
