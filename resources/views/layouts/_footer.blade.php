40759a7f7378ed5c1638bbee6f8baa83442fbded
k155 <hy_kissss@163.com>
2018-04-24 04:16:13 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://github.com/hykissss/larabbs" target="_blank">Ki55</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:hyuuu1024@gmail.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
